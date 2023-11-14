7cd8c0c3b996dca6f4c2af31c90ba48b54388cf8
lucasmoraisnt <rm93267@fiap.com.br>
2023-11-14 11:11:49 -0300

    Jogo finalizado

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-var nome = document.querySelector('#name')
-
-var cotacaoDolar = 4.9;
-var cotacaoEthereum = 10197.89;
-
-var anosLuz = 9460000000000
-var metros = 2
-
-var valorParaConverter = 30;
-
-function converteDolar(){
-    var valorConvertido = valorParaConverter*cotacaoDolar;
-    alert(`Olá ${nome.value}. Esse é o valor convertido - R$ ${valorConvertido.toFixed(2)}`);
+function numberRandom(min, max){
+    return parseInt(Math.random() * (max - min) + min);
 }
 
-function converteEthereum(){
-    var valorConvertido = valorParaConverter*cotacaoEthereum;
-    alert(`Olá ${nome.value}. 1 Ethereum equivale a R$ ${cotacaoEthereum} e ETH ${valorParaConverter} equivale a R$ ${valorConvertido.toFixed(2)}`)
+var numeroSecreto = numberRandom(1, 1001);
+
+for(var i = 1; i<=10; i++){
+    var chute = prompt("Digite um número entre 1 e 1000: ")
+    if(chute == numeroSecreto){
+        alert(`Parabéns você acerto. O número é ${numeroSecreto} \nQuantidade de chutes: ${i}`)
+    }else if(chute > numeroSecreto){
+        alert(`Errou... o número é menor que ${chute} \nQuantidade de chutes: ${i}`)
+    }else if(chute < numeroSecreto){
+        alert(`Errou... o número é maior que ${chute} \nQuantidade de chutes: ${i}`)
+    } 
 }
 
-function converteAnosLuz(){
-    var valorConvertido = metros*anosLuz;
-    alert(`Olá ${nome.value}. Em metros 1 ano luz equivale a ${anosLuz}m e ${metros}m equivale a ${valorConvertido.toFixed(2)}m`)
-}
+alert(`Você perdeu todas tentativas - O número era ${numeroSecreto}`)
