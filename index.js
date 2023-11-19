3fe9f2b047a83ee7bcd4d5382c2b12c5315bd747
lukspg <RM93267@fiap.com>
2023-11-18 23:00:58 -0300

    Adicionando imagem e trailer dos filmes em uma lista de objetos e rodando esse lista com loop for

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
+var listaFilmes = []
+
 function adicionarFilme() {
     var urlImagem = document.getElementById("filme").value;
     var urlTrailer = document.querySelector("#trailer").value;
     var divListaFilmes = document.getElementById("listaFilmes");
+   
     var jpg = ".jpg"
     var png = ".png"
 
     if (urlImagem.includes(jpg) || urlImagem.includes(png)) {
-        divListaFilmes.innerHTML += `
-            <a href="${urlTrailer}" target="_blank">
-                <img src="${urlImagem}">
-            </a>
-        `;
-    } else {
+        listaFilmes.push({ imagem: urlImagem, trailer: urlTrailer });
+
+        divListaFilmes.innerHTML = "";
+
+        for (var i = 0; i < listaFilmes.length; i++) {
+            divListaFilmes.innerHTML += `
+                <a href="${listaFilmes[i].trailer}" target="_blank">
+                    <img src="${listaFilmes[i].imagem}">
+                </a>
+            `;
+        } 
+    }else {
         alert("Digite uma opção valida")
     }
 
